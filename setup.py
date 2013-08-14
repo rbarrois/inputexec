02e7440315bdfc447c718a1beb8a78db4bd8cfe7
Raphaël Barrois <raphael.barrois@polytechnique.org>
2013-08-14 18:07:01 +0200

    Fix setup.py post-setuptools/distribute merge.

--- setup.py
+++ setup.py
@@ -5,103 +5,61 @@
 
 import os
 import re
-import sys
-from distutils.core import setup
-from distutils import cmd
 
-root = os.path.abspath(os.path.dirname(__file__))
-PACKAGE = 'inputexec'
+from setuptools import setup
 
-def get_version(*module_dir_components):
-    version_re = re.compile(r"^__version__ = ['\"](.*)['\"]$")
-    module_root = os.path.join(root, *module_dir_components)
-    module_init = os.path.join(module_root, '__init__.py')
-    with open(module_init, 'r') as f:
+root_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version(package_name):
+    version_re = re.compile(r"^__version__ = [\"']([\w_.-]+)[\"']$")
+    package_components = package_name.split('.')
+    path_components = package_components + ['__init__.py']
+    with open(os.path.join(root_dir, *path_components)) as f:
         for line in f:
             match = version_re.match(line[:-1])
             if match:
                 return match.groups()[0]
     return '0.1.0'
 
-VERSION = get_version(PACKAGE)
 
-
-class test(cmd.Command):
-    """Run the tests for this package."""
-    command_name = 'test'
-    description = 'run the tests associated with the package'
-
-    user_options = [
-        ('test-suite=', None, "A test suite to run (defaults to 'tests')"),
-    ]
-
-    def initialize_options(self):
-        self.test_runner = None
-        self.test_suite = None
-
-    def finalize_options(self):
-        self.ensure_string('test_suite', 'tests')
-
-    def run(self):
-        """Run the test suite."""
-        try:
-            import unittest2 as unittest
-        except ImportError:
-            import unittest
-
-        import logging
-        logger = logging.getLogger(PACKAGE)
-        logger.addHandler(logging.StreamHandler())
-
-        verbosity = self.verbose
-        if verbosity >= 2:
-            logger.setLevel(logging.DEBUG)
-        else:
-            logger.setLevel(logging.INFO)
-
-        loader = unittest.TestLoader()
-        suite = unittest.TestSuite()
-
-        if self.test_suite == 'tests':
-            for test_module in loader.discover('.'):
-                suite.addTest(test_module)
-        else:
-            suite.addTest(loader.loadTestsFromName(self.test_suite))
-
-        result = unittest.TextTestRunner(verbosity=verbosity).run(suite)
-        if not result.wasSuccessful():
-            sys.exit(1)
+PACKAGE = 'inputexec'
 
 
 setup(
     name=PACKAGE,
-    version=VERSION,
-    description="Simple Python program to execute commands on keypress on headless systems",
+    version=get_version(PACKAGE),
     author="Raphaël Barrois",
-    author_email='raphael.barrois+inputexec@polytechnique.org',
-    url='https://github.com/rbarrois/inputexec',
-    keywords=['input', 'exec', 'command', 'evdev', 'event'],
+    author_email="raphael.barrois+inputexec@polytechnique.org",
+    description="Simple Python program to execute commands on keypress on headless systems",
+    keywords=["input", "exec", "command", "evdev", "event"],
+    license="BSD",
+    url="https://github.com/rbarrois/inputexec",
     packages=[
         'inputexec', 'inputexec.readers',
     ],
     scripts=['bin/inputexec'],
-    license='BSD',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: No Input/Output (Daemon)',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Topic :: Utilities',
+    setup_requires=[
+        'setuptools>=0.8',
     ],
-    cmdclass={'test': test},
+    install_requires=[
+        'evdev',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: No Input/Output (Daemon)",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Utilities",
+    ],
+    test_suite='tests',
 )
-
