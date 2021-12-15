9e45d4dac0c6c42b503e580414a0f4fd414009eb
Raphaël Barrois <raphael.barrois@polytechnique.org>
2021-12-15 09:59:35 +0100

    Update copyright years

--- inputexec/__init__.py
+++ inputexec/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2013 Raphaël Barrois
+# Copyright (c) 2013-2021 Raphaël Barrois
 # This code is distributed under the 2-clause BSD License.
 
 from __future__ import unicode_literals
